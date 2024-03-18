0d99a432b52746b0383d4b33aea780eac604718d
Sparsh Verma <sparshv70@gmail.com>
2024-03-18 12:58:36 +0530

    CORS Error in production but not in dev

--- backend/utilities/allowed-origins.ts
+++ backend/utilities/allowed-origins.ts
@@ -2,4 +2,5 @@ export const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
   "http://localhost:3002",
+  "https://pdf-extractor-frontend.vercel.app",
 ];
--- backend/utilities/cors-options.ts
+++ backend/utilities/cors-options.ts
@@ -4,6 +4,7 @@ import { allowedOrigins } from "./allowed-origins";
 export const corsOptions: CorsOptions = {
   origin: (origin: any, callback: any) => {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      console.log("Allowed by CORS");
       callback(null, true);
     } else {
       callback(new Error("Not allowed as it is bloked by CORS"));
